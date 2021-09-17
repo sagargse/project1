ed01d1faf628fd241f8fe518d43b0333e817c8db
sagarraj <>
2021-09-17 12:59:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+var readlineSync = require("readline-sync")
+var score=0;
+var username = readlineSync.question("Hey! what is your name : ")
+console.log("Hello! "+ username + " do you know SAGAR RAJ ? YES/NO ")
+var answer = readlineSync.question(answer)
+var answer1 = answer.toLowerCase()
+console.log("-------------------")
+if(answer1 === 'yes')
+{
+function knowsagar(que,ans){
+var useranswer = readlineSync.question(que)
+if(useranswer.toLowerCase() === ans){
+console.log("Great! you are right");
+score +=1;
+}
+else{
+console.log("Ohh! Wrong"); 
+}
+console.log("Current Score = ",score);
+console.log("-------------------")
+}
+}
+else if (answer1 === 'no')
+{
+console.log("It's OK! Have a great day")
+console.log("-------------------")
+}
+if(answer1 === "yes")
+{
+var knowme =[
+  {que:"Where do he live? ",
+  ans:"patna"},
+  {que:"Sagar mostly vists macd/kfc? ",
+  ans:"kfc"},
+  {que:"Sagar likes Mountains/beaches? ",
+  ans: "mountains"}
+  ];
+
+for(var i=0;i<knowme.length;i++){
+  var currentque = knowme[i];
+  knowsagar(currentque.que,currentque.ans)
+}
+console.log("You scored : ", score )
+console.log("Thanks! For Your Time See You Bye-Bye")
+}
+
+
